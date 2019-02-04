501eb43e158333b3b6f11111e2f15f452aaac7f1
SerovaEkaterina <43706821+SerovaEkaterina@users.noreply.github.com>
2019-02-04 13:02:37 +0500

    food

---
+++ FoodList/app/src/main/java/com/example/lenovo/foodlist/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.example.lenovo.foodlist.utils;
+
+public class Constants {
+
+    public static final String foodDescriptionExtra = "foodDescription";
+    public static final String countryNameExtra = "countryName";
+    public static final String foodNameExtra = "foodName";
+    public static final String foodPhotoResIdExtra = "foodPhotoResId";
+}
